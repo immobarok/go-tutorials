53dd1024505ae58f00485c683b54a86551166a63
immobarok <immobarokali2001@gmail.com>
2023-07-25 23:06:13 +0600

    lecture -3

---
+++ lecture_2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+func main(){
+	fullName := "Mobarok"
+	country := "Bangladesh"
+	age := 21
+	gpa := 5
+	
+	fmt.Println("Name :",fullName)
+	fmt.Println("Country :",country)
+	fmt.Println("Age :",age)
+	fmt.Println("GPA :",gpa)
+}
---
+++ lecture_3.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+ func main(){
+	fullName := "Mobarok"
+	age := 21
+	fmt.Printf("%v is a student\n",fullName)
+	fmt.Printf("He is %v years old\n",age)
+ }
