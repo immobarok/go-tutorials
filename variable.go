ba6022132ebb63479233459cb73dbf7d22e25718
immobarok <immobarokali2001@gmail.com>
2023-07-25 22:53:55 +0600

    varibale-2

---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+func main(){
+	var fullName string = "Mobarok"
+	var country string = "Bangladesh"
+	var age int = 32
+	var gpa float32 = 5
+	fmt.Println(fullName,"is a student")
+	fmt.Println(fullName,"is",age," years old")
+	fmt.Println(fullName,"has got",gpa,"is a student")
+	fmt.Println(fullName,"is orginally from",country)
+}
